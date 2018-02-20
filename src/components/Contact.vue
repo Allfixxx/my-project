b3f0a64c763df19eb9c931e75e64e597d2c7ca89
Nicolai Øllegaard Hansen <Allfix93@gmail.com>
2018-02-20 04:40:34 +0100

    Contact page now working
    and made some minor changes in header and footer

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,71 +1,70 @@
 <template>
 
-  <form id="app" @submit="checkForm" action="https://vuejs.org/" method="post" novalidate="true">
+  <div class="wrapper">
 
-    <p v-if="errors.length">
-      <b>Please correct the following error(s):</b>
-      <ul>
-        <li v-for="error in errors">{{ error }}</li>
-      </ul>
-    </p>
+    <div class="navBackground"></div>
 
-    <p>
-      <label for="name">
-        Name</label>
-          <input type="text" name="name" id="name" v-model="name">
-    </p>
+    <div class="contactForm">
 
-    <p>
-      <label for="email">
-        Email</label>
-          <input type="email" name="email" id="email" v-model="email">
-    </p>
+      <form @submit.prevent="submitForm" novalidate v-if="!isSubmitted">
 
-    <p>
-      <label for="movie">
-        Favorite Movie</label>
-          <select name="movie" id="movie" v-model="movie">
-            <option>Star Wars</option>
-            <option>Vanilla Sky</option>
-            <option>Atomic Blonde</option>
+        <div>
+          <label for="firstname">First name</label>
+          <input type="text" v-model.lazy="form.firstname" id="firstname" v-validate="'required'" name="firstname">
+          <span v-show="errors.has('firstname')" class="help is-danger">{{ errors.first('firstname') }} </span>
+        </div>
+
+        <div>
+          <label for="email">Email</label>
+          <input type="text" v-model.lazy="form.email" id="email" v-validate="'required|email'" name="email">
+          <span v-show="errors.has('email')" class="help is-danger">{{ errors.first('email') }}</span>
+        </div>
+        <div>
+          <label for="subject">Choose a subject</label>
+          <select name="subject" id="subject" v-model="subject">
+            <option>Problem with payment</option>
+            <option>Problem with an order</option>
+            <option>Other</option>
           </select>
-    </p>
+        </div>
+        <div>
+          <label for="email">Email</label>
+          <textarea rows="6" cols="75" type="text" v-model.lazy="form.textarea" id="textarea" v-validate="'required'" name="textarea"></textarea>
+          <span v-show="errors.has('textarea')" class="help is-danger">{{ errors.first('textarea') }}</span>
+        </div>
 
-    <p>
-      <input type="submit" value="Submit">
-    </p>
+        <button type="submit" :disabled="errors.any()">Submit</button>
 
-  </form>
+      </form>
+
+      <div v-else>
+        Thanks for submitting!
+      </div>
+
+    </div>
+  </div>
 
 </template>
 
 <script>
-  const app = new Vue({
-    el: '#app',
-    data: {
-      errors: [],
-      name: null,
-      email: null,
-      movie: null
+  export default {
+    name: 'my-form',
+    data: function () {
+      return {
+        form: {
+          firstname: '',
+          email: ''
+        },
+        isSubmitted: false
+      }
     },
     methods: {
-      checkForm: function (e) {
-        this.errors = [];
-        if (!this.name) this.errors.push("Name required.");
-        if (!this.email) {
-          this.errors.push("Email required.");
-        } else if (!this.validEmail(this.email)) {
-          this.errors.push("Valid email required.");
-        }
-        if (!this.errors.length) return true;
-        e.preventDefault();
-      },
-      validEmail: function (email) {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
+      submitForm: function () {
+        this.isSubmitted = true;
+        this.form = null;
       }
     }
-  })
+  }
 </script>
 
 
